470ad82ac4efe50017f29c95ba2bf929e2d8b608
huliangbo <xc_qin@163.com>
2017-12-07 14:27:57 +0800

    xc_qin

--- ddtong-parent/ddtong-app/src/main/java/com/ddtong/controller/message/SmsController.java
+++ ddtong-parent/ddtong-app/src/main/java/com/ddtong/controller/message/SmsController.java
@@ -1,12 +1,22 @@
 package com.ddtong.controller.message;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.ddtong.core.common.DdtongConstant;
+import com.ddtong.core.vo.ApiResponseResult;
 
 @Controller
 @RequestMapping(DdtongConstant.APP_API_PATH + "/sms/")
 public class SmsController {
 
+	@RequestMapping("send")
+	@ResponseBody
+	public ApiResponseResult send(HttpServletRequest request, HttpServletResponse response){
+		return null;
+	}
 }
