99f97e15c6abb7b03b1ca128a01287d0cb4a1db7
lihangang <372397387@qq.com>
2017-12-11 10:40:43 +0800

    更新登录密码,支付密码

--- ddtong-parent/ddtong-core/src/main/java/com/ddtong/core/dao/mapper/custom/CustomInfoMapper.java
+++ ddtong-parent/ddtong-core/src/main/java/com/ddtong/core/dao/mapper/custom/CustomInfoMapper.java
@@ -2,6 +2,7 @@ package com.ddtong.core.dao.mapper.custom;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Update;
 
 import com.ddtong.core.entity.custom.TCustom;
 
@@ -9,4 +10,10 @@ import com.ddtong.core.entity.custom.TCustom;
 public interface CustomInfoMapper {
 	
 	TCustom selectOneByid(@Param("id") long id);
+	
+	@Update(value = "update t_custom set `password`=#{password}, update_time=NOW() where id=#{id}")
+	int updatePwd(@Param("id") long id, @Param("password") String password);
+
+	@Update(value = "update t_custom set `pay_password`=#{payPassword}, update_time=NOW() where id=#{id}")
+	int updatePayPwd(@Param("id") long id, @Param("payPassword") String payPassword);
 }
--- ddtong-parent/ddtong-core/src/main/java/com/ddtong/core/dao/mapper/merchant/MerchantInfoMapper.java
+++ ddtong-parent/ddtong-core/src/main/java/com/ddtong/core/dao/mapper/merchant/MerchantInfoMapper.java
@@ -2,12 +2,18 @@ package com.ddtong.core.dao.mapper.merchant;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Update;
 
-import com.ddtong.core.entity.custom.TCustom;
 import com.ddtong.core.entity.merchant.TMerchant;
 
 @Mapper
 public interface MerchantInfoMapper {
-	
+
 	TMerchant selectOneByid(@Param("id") long id);
+
+	@Update(value = "update t_merchant set `password`=#{password}, update_time=NOW() where id=#{id}")
+	int updatePwd(@Param("id") long id, @Param("password") String password);
+
+	@Update(value = "update t_merchant set `pay_password`=#{payPassword}, update_time=NOW() where id=#{id}")
+	int updatePayPwd(@Param("id") long id, @Param("payPassword") String payPassword);
 }
